8d75ede1f67d7f072c17c80d05915577df05bd99
johnzw <rzzw456@126.com>
2014-02-04 19:13:27 -0700

    modify something

--- src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
+++ src/ca/ualberta/cs/comput301/currentlocation/CurrentLocationActivity.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cs.comput301.currentlocation;
 
+import java.sql.Date;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,6 +12,7 @@ import android.view.Menu;
 import android.widget.TextView;
 
 public class CurrentLocationActivity extends Activity {
+	TextView tv;
 	private static final String GPS_MOCK_PROVIDER = "GpsMockProvider";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +22,41 @@ public class CurrentLocationActivity extends Activity {
 		
 		// Obtain LocationManager service and set up location update request.
 		// To Do
+		tv = (TextView)findViewById(R.id.myLocationText);
+		LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
 	}
 
 	
 	// Retrieve location updates through LocationListener interface
 	private final LocationListener locationListener = new LocationListener(){
 	// To Do: override the four methods.
-	}	
+		public void onLocationChanged (Location location){
+			if(location!=null){
+				double lat = location.getLatitude();
+				double lng = location.getLongitude();
+				Date date = new Date(location.getTime());
+				tv.setText("Your location is:"+lat+" "+lng+" "+"\ntime"+date);
+			}
+			else{
+				tv.setText("No");
+			}
+		}
+		public void onProviderDisabled (String provider){
+			
+		}
+		public void onProviderEnabled (String provider){
+			
+		}
+		public void onStatusChanged (String provider){
+			
+		}
+		@Override
+		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+	};	
 
 	
 	@Override
